a36c3ef12a1a0c9cc63d987fcd751c7733daa28e
Zhasmin <jas.hovhannisyan@gmail.com>
2025-02-24 03:41:57 +0400

    initial commit

--- tic-tac-toe/src/tic_tac_toe.py
+++ tic-tac-toe/src/tic_tac_toe.py
@@ -19,33 +19,45 @@ def train(epochs: int, print_every_n: int = 500):
     # region Body
 
     # Create 2 RL players with ε = 0.01 exploring probability
-
+    player1 = RLPlayer(all_states, epsilon=0.01)
+    player2 = RLPlayer(all_states, epsilon=0.01)
 
     # Create a judge to organize the game
-
+    judge = Judge(player1, player2)
 
     # Set the initial win rate of both players to 0
-
+    p1_win_rate = 0
+    p2_win_rate = 0
 
     # For every epoch
+    for epoch in range(1, 1 + epochs):
+
 
         # get the winner
 
+        winner = judge.play(all_states)
 
         # check which player is the winner
-
+        if winner == 1:
+            p1_win_rate += 1
+        if winner == -1:
+            p2_win_rate += 1
 
         # print the intermediate win rates, if needed
+        if epoch % print_every_n == 0:
+            print(f'epoch n:{epoch}, win rate player 1:{p1_win_rate}, win rate player 2:{p2_win_rate}')
 
 
         # update value estimates of both players
-
+        player1.update_state_value_estimates()
+        player2.update_state_value_estimates()
 
         # reset the judge => players
-
+        judge.reset()
 
     # Save the players' policies
-
+    player1.save_policy()
+    player2.save_policy()
 
     # endregion Body
 
@@ -61,27 +73,35 @@ def compete(turns):
     # region Body
 
     # Create 2 RL players with ε = 0 exploring probability (i.e. greedy)
-
+    player1 = RLPlayer(all_states, epsilon=0)
+    player2 = RLPlayer(all_states, epsilon=0)
 
     # Create a judge to organize the game
-
+    judge = Judge(player1, player2)
 
     # Load the players' policies
-
+    player1.load_policy()
+    player2.load_policy()
 
     # Set the initial win rate of both players to 0
-
+    p1_win_rate = 0
+    p2_win_rate = 0
 
     # For every turn
+    for turn in range(turns):
+
 
         # get the winner
-
+        winner = judge.play(all_states)
 
         # check which player is the winner
-
+        if winner == 1:
+            p1_win_rate += 1
+        if winner == -1:
+            p2_win_rate += 1
 
         # reset the judge => players
-
+        judge.reset()
 
 
 
@@ -100,22 +120,27 @@ def play():
 
     while True:
         # Create a human player
-
+        human_player = HumanPlayer()
 
         # Create RL player with ε = 0 exploring probability (i.e. greedy)
-
+        rl_player = RLPlayer(all_states, epsilon=0)
 
         # Create a judge to organize the game
-
+        judge = Judge(human_player, rl_player)
 
         # Load the RL player's policy
-
+        rl_player.load_policy()
 
         # Get the winner
-
+        winner = judge.play(all_states)
 
         # Check which player is the winner
-
+        if winner == 1:
+            print('Victory!')
+        elif winner == -1:
+            print('Game over!')
+        else:
+            print("It's a tie")
 
     # endregion Body
 
